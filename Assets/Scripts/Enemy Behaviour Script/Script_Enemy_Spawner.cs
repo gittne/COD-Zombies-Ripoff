e3daac1eff6cace8bb3e042885732487a8b7dfb3
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2024-02-12 03:02:55 +0100

    enemy spawner varibles

--- Assets/Scripts/Enemy Behaviour Script/Script_Enemy_Spawner.cs
+++ Assets/Scripts/Enemy Behaviour Script/Script_Enemy_Spawner.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Script_Enemy_Spawner : MonoBehaviour
 {
+
+    public int currentWaveIndex {  get; private set; } //A current wave number
+
+    [SerializeField] GameObject[] Horde;
+
+    [SerializeField] int waveAmount; // The Amount of waves in total
+    [SerializeField] int enemiesLeft; // The amount of enemies still alive 
+    [SerializeField] int currentHordeAmount; //the total amount of enemies in that round
+  
+
     // Start is called before the first frame update
     void Start()
     {
