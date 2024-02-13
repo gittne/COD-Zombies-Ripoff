2e54a50a0449d02eb0642d4de7a8ace4502889cf
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2024-02-13 04:10:48 +0100

    Enamy spawner and bens folders

--- Assets/Scripts/Enemy Behaviour Script/Script_Enemy_Spawner.cs
+++ Assets/Scripts/Enemy Behaviour Script/Script_Enemy_Spawner.cs
@@ -2,37 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Script_Enemy_Spawner : MonoBehaviour
 {
-
+    public enum SpawnState { SPAWNING, WAITING, COUNTING}
     public int currentWaveIndex {  get; private set; } //A current wave number
+    [SerializeField] Wave[] waves;
 
-    public Wave[] waves;
+    [SerializeField] float timeBetweenWaves = 5f;
+    [SerializeField] int nextWave = 0;
+    [SerializeField] float countDown;
+  
 
+    private float searchCountDown = 3f;
+
+
+    private SpawnState state = SpawnState.WAITING;
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public GameObject[] Horde;// The amount of enemies on the board
+        public float spawnRate;
+        //public int waveAmount; // The Amount of waves in total
+        public int currentHordeAmount; //the total amount of enemies in that round
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     countDown = timeBetweenWaves;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive()) // check if enemy is dead
+            {
+                Debug.Log("Round Over");
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (countDown <= 0)
+        {
+            if (true)
+            {
+                if (state != SpawnState.SPAWNING)
+                {
+                    StartCoroutine(SpawnWave(waves[nextWave]));// starts the spawning
+                }
+            }
+        }
+        else
+        {
+          countDown -= timeBetweenWaves;    
+        }
+
     }
-
-    [System.Serializable]
-    public class Wave
+    bool EnemyIsAlive()
     {
-      public string waveName; 
-      public GameObject[] Horde;// The amount of enemies on the board
-      public float SpawnSpeed;
+        searchCountDown -= Time.deltaTime;
 
-      public int waveAmount; // The Amount of waves in total
-      public int enemiesLeft; // The amount of enemies still alive 
-      public int currentHordeAmount; //the total amount of enemies in that round
-      
+        if (searchCountDown <= 0)
+        {
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.currentHordeAmount; i++)   // Spawning
+        {
+            Instantiate(_wave.Horde[i], transform.position, transform.rotation);
+            yield return new WaitForSeconds(1f/_wave.spawnRate);
+        }                                  
+
+        state = SpawnState.WAITING;
+
+        yield break;
     }
 
 }
